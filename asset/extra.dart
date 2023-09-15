a5a3f83284e1d3fb9549f0856275c860af1d6ec1
Hiral patel <drashti.sohamtech@gmail.com>
2023-09-15 16:59:53 +0530

    lode error

--- asset/extra.dart
+++ asset/extra.dart
@@ -1,33 +1,155 @@
+// import 'dart:async';
 // import 'dart:io';
-
-// import 'package:file_picker/file_picker.dart';
 // import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:image_picker/image_picker.dart';
+// import 'package:flutter_test/flutter_test.dart';
 // import 'package:permission_handler/permission_handler.dart';
+// import 'package:photo_gallery/photo_gallery.dart';
+// import 'package:transparent_image/transparent_image.dart';
 
 // void main() {
 //   runApp(MyApp());
 // }
 
-// class ImageController extends GetxController {
-//   final RxList<XFile> images = <XFile>[].obs;
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
 // }
 
-// class MyApp extends StatelessWidget {
+// class _MyAppState extends State<MyApp> {
+//   // List<Album>? _albums;
+//   List<Medium>? _media;
+//   bool _loading = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _loading = true;
+//     initAsync();
+//   }
+
+//   Future<void> initAsync() async {
+//     if (await _promptPermissionSetting()) {
+//       List<Album> albums = await PhotoGallery.listAlbums();
+//       List<Medium> allMedia = [];
+//       for (var album in albums) {
+//         MediaPage mediaPage = await album.listMedia();
+//         allMedia.addAll(mediaPage.items);
+//       }
+//       setState(() {
+//         // _albums = albums;
+//         _loading = false;
+//         _media = allMedia; // Assign _media here
+//       });
+//     } else {
+//       setState(() {
+//         _loading = false;
+//       });
+//     }
+//   }
+
+//   Future<bool> _promptPermissionSetting() async {
+//     if (Platform.isIOS) {
+//       if (await Permission.photos.request().isGranted ||
+//           await Permission.storage.request().isGranted) {
+//         return true;
+//       }
+//     }
+//     if (Platform.isAndroid) {
+//       if (await Permission.storage.request().isGranted ||
+//           await Permission.photos.request().isGranted &&
+//               await Permission.videos.request().isGranted) {
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+
 //   @override
 //   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       title: 'Gallery App',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Photo gallery example'),
+//         ),
+//         body: _loading
+//             ? const Center(
+//                 child: CircularProgressIndicator(),
+//               )
+//             : LayoutBuilder(
+//                 builder: (context, constraints) {
+//                   double gridWidth = (constraints.maxWidth - 20) / 3;
+//                   double gridHeight = gridWidth + 33;
+//                   double ratio = gridWidth / gridHeight;
+//                   return Container(
+//                     padding: const EdgeInsets.all(5),
+//                     child: GridView.count(
+//                       crossAxisCount: 3,
+//                       mainAxisSpacing: 1.0,
+//                       crossAxisSpacing: 1.0,
+//                       children: <Widget>[
+//                         ...?_media?.map(
+//                           (medium) => GestureDetector(
+//                             onTap: () => Navigator.of(context).push(
+//                               MaterialPageRoute(
+//                                   builder: (context) => ViewerPage(medium)),
+//                             ),
+//                             child: Container(
+//                               color: Colors.grey[300],
+//                               child: FadeInImage(
+//                                 fit: BoxFit.cover,
+//                                 placeholder: MemoryImage(kTransparentImage),
+//                                 image: ThumbnailProvider(
+//                                   mediumId: medium.id,
+//                                   mediumType: medium.mediumType,
+//                                   highQuality: true,
+//                                 ),
+//                               ),
+//                             ),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   );
+//                 },
+//               ),
 //       ),
-//       home: HomeScreen(),
 //     );
 //   }
 // }
 
-// class HomeScreen extends StatelessWidget {
-  
+// class ViewerPage extends StatelessWidget {
+//   final Medium medium;
+
+//   ViewerPage(Medium medium) : medium = medium;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     DateTime? date = medium.creationDate ?? medium.modifiedDate;
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           leading: IconButton(
+//             onPressed: () => Navigator.of(context).pop(),
+//             icon: const Icon(Icons.arrow_back_ios),
+//           ),
+//           title: date != null ? Text(date.toLocal().toString()) : null,
+//         ),
+//         body: Container(
+//           alignment: Alignment.center,
+//           child: medium.mediumType == MediumType.image
+//               ? GestureDetector(
+//                   onTap: () async {
+//                     PhotoGallery.deleteMedium(mediumId: medium.id);
+//                   },
+//                   child: FadeInImage(
+//                     fit: BoxFit.cover,
+//                     placeholder: MemoryImage(kTransparentImage),
+//                     image: PhotoProvider(mediumId: medium.id),
+//                   ),
+//                 )
+//               : null,
+//         ),
+//       ),
+//     );
 //   }
 // }
--- lib/bottom_app_bar/bottom_navbar.dart
+++ lib/bottom_app_bar/bottom_navbar.dart
@@ -1,9 +1,10 @@
 import 'package:art_gallery_app_ui/screens/explore/explore_screen.dart';
-import 'package:art_gallery_app_ui/screens/home_screen.dart';
+import 'package:art_gallery_app_ui/screens/folder/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:floating_bottom_bar/animated_bottom_navigation_bar.dart';
-import '../screens/global_screen.dart';
+import '../screens/home/global_screen.dart';
+import '../screens/setting/setting_screen.dart';
 import 'bottom_navbar_cnt.dart';
 
 class BottomNavBar extends StatefulWidget {
@@ -15,79 +16,84 @@ class BottomNavBar extends StatefulWidget {
 
 class _BottomNavBarState extends State<BottomNavBar> {
   final List<Widget> pages = [
-    ExploreScreen(),
+    const ExploreScreen(),
     const HomePage(),
     const GlobalPage(),
+    SettingsPage(),
 
-    // Other pages you may want to include
+
   ];
-
   BottomNavController controller = Get.put(BottomNavController());
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      body: Obx(
-        () => Center(
-          child: pages.elementAt(controller.currentIndex.value),
+      child: Scaffold(
+        body: Obx(
+          () => Center(
+            child: pages.elementAt(controller.currentIndex.value),
+          ),
         ),
-      ),
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        controller: FloatingBottomBarController(initialIndex: 0),
-        bottomBar: [
-          BottomBarItem(
-            icon: const Icon(Icons.explore_outlined, size: 25),
-            iconSelected: const Icon(Icons.explore_outlined,
-                color: AppColors.cherryRed, size: 25),
-            title: 'Photos',
-            dotColor: AppColors.cherryRed,
-            onTap: (value) {
-              controller.changePage(0);
-            },
-          ),
-          BottomBarItem(
-            icon: const Icon(Icons.border_all_rounded, size: 25),
-            iconSelected: const Icon(Icons.border_all_rounded,
-                color: AppColors.cherryRed, size: 25),
-            title: 'Folder',
-            dotColor: AppColors.cherryRed,
-            onTap: (value) {
-              controller.changePage(1);
-            },
-          ),
-          BottomBarItem(
-            icon: const Icon(Icons.gps_fixed_rounded, size: 25),
-            iconSelected: const Icon(Icons.gps_fixed_rounded,
-                color: AppColors.cherryRed, size: 25),
-            title: 'Global',
-            dotColor: AppColors.cherryRed,
-            onTap: (value) {
-              controller.changePage(2);
-            },
-          ),
-          BottomBarItem(
-            icon: const Icon(Icons.person_2_outlined, size: 25),
-            iconSelected: const Icon(Icons.person_2_outlined,
-                color: AppColors.cherryRed, size: 25),
-            title: 'search',
-            dotColor: AppColors.cherryRed,
-            onTap: (value) {
-              controller.changePage(3);
-            },
-          ),
-        ],
-        bottomBarCenterModel: BottomBarCenterModel(
-          centerBackgroundColor: AppColors.cherryRed,
-          centerIcon: const FloatingCenterButton(
-            
-            child: Icon(
-              Icons.add,
-              color: AppColors.white,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your action for the floating action button here
+          },
+          child: const Icon(Icons.add),
+          backgroundColor: AppColors.cherryRed,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: Obx(
+          () => BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            notchMargin: 4.0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.explore_outlined),
+                    onPressed: () {
+                      controller.changePage(0);
+                    },
+                    color: controller.currentIndex.value == 0
+                        ? AppColors.cherryRed // Set the selected color here
+                        : AppColors.black, //  // Set the unselected color here
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.border_all_rounded),
+                    onPressed: () {
+                      controller.changePage(1);
+                    },
+                    color: controller.currentIndex.value == 1
+                        ? AppColors.cherryRed // Set the selected color here
+                        : AppColors.black, // Set the unselected color here
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.gps_fixed_rounded),
+                    onPressed: () {
+                      controller.changePage(2);
+                    },
+                    color: controller.currentIndex.value == 2
+                        ? AppColors.cherryRed // Set the selected color here
+                        : AppColors.black, //  // Set the unselected color here
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.person_2_outlined),
+                    onPressed: () {
+                      controller.changePage(3);
+                    },
+                    color: controller.currentIndex.value == 3
+                        ? AppColors.cherryRed // Set the selected color here
+                        : AppColors.black, // Set the unselected color here
+                  ),
+                ],
+              ),
             ),
           ),
-          centerIconChild: [],
         ),
       ),
-    ));
+    );
   }
 }
